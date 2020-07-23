80ad2c8de84473e28ade361ce467129cbc37701c
clement.san@gmail.com <clement.san@gmail.com>
2020-07-23 14:25:48 +0200

    add config template

---
+++ config_template.py
@@ -0,0 +1,6 @@
+#fill your own AWS API KEY and bucket name here, then rename the file as config.py
+ROOT_URL ="https://www.franceculture.fr/emissions/{0}?p={1}"
+AWS_SERVER_PUBLIC_KEY =""
+AWS_SERVER_SECRET_KEY =""
+bucket_name = ""
+range_url = range(0,300)
